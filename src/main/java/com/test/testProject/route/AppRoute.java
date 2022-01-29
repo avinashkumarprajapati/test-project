b84a0e41fc202cbe549abfb457f1d89cd74f84dd
avinashkumarprajapati <avinashkumarprajapati@gmail.com>
2022-01-30 00:35:19 +0530

    dummy

--- src/main/java/com/test/testProject/route/AppRoute.java
+++ src/main/java/com/test/testProject/route/AppRoute.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppRoute {
-
     @GetMapping("/returnHello")
     public String returnHello() {
         return "Hello Avinash!";
