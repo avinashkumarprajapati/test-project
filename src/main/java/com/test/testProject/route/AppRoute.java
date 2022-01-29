fa775d9c47a6cebec6d44753786784acfc4b283b
avinashkumarprajapati <avinashkumarprajapati@gmail.com>
2022-01-30 00:12:01 +0530

    route added

---
+++ src/main/java/com/test/testProject/route/AppRoute.java
@@ -0,0 +1,13 @@
+package com.test.testProject.route;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppRoute {
+
+    @GetMapping("/returnHello")
+    public String returnHello() {
+        return "Hello Avinash!";
+    }
+}
